f22a350509f1348fbae6aa2fb85303e15cda0eda
Devin Tripp <devinshawntripp@gmail.com>
2024-09-17 19:05:49 -0500

    edited entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 echo "Starting entrypoint.sh"
 # Wait for the database to be ready
-./wait-for-it.sh db:5432 -t 60
+echo "Waiting for the database to be ready..."
+until nc -z -v -w30 db 5432
+do
+  echo "Waiting for database connection..."
+  sleep 1
+done
+echo "Database is up and running!"
 
 # Reset the database (this will drop all tables and recreate them)
 npx prisma db push --force-reset
