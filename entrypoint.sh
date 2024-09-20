1358857952a7f9300e2bc33ee3c4aee6453780c6
Devin Tripp <devinshawntripp@gmail.com>
2024-09-20 00:37:22 -0500

    check db conn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 echo "Starting entrypoint.sh"
 # Wait for the database to be ready
 echo "Waiting for the database to be ready..."
+if [ "$CHECK_DB_CONNECTION" = "true" ]; then
 until nc -z -v -w30 db 5432
 do
   echo "Waiting for database connection..."
   sleep 1
 done
+fi
 echo "Database is up and running!"
 
 # Deploy migrations
